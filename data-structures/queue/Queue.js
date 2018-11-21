939b0e88c9d6f2f33c8b9efc5f691d42ab7c5a36
Sergei Grin <grinzzly@gmail.com>
2018-11-21 16:39:05 +0100

    [DS] Queue

---
+++ data-structures/queue/Queue.js
@@ -0,0 +1,35 @@
+class Queue {
+  constructor(value) {
+    this.data = [value];
+  }
+
+  add(value) {
+    this.data.unshift(value);
+  }
+
+  remove() {
+    this.data.pop();
+  }
+
+  getFirst() {
+    return this.data[0];
+  }
+
+  getLast() {
+    return this.data[this.data.length - 1];
+  }
+
+  getSize() {
+    return this.data.length;
+  }
+}
+
+const queue = new Queue();
+
+queue.add(1);
+queue.add(2);
+queue.add(3);
+queue.remove();
+console.log('The wWhole Queue: ', queue);
+console.log('Size of the Queue: ', queue.getSize());
+
