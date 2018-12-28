e71a9ad395ed10efaadf743340b08bdc90034b08
Sergei Grin <grinzzly@gmail.com>
2018-12-28 16:36:08 +0100

    [AL] Z Algorithm

--- algorithms/strings/z-algorithm/zAlgorithm.js
+++ algorithms/strings/z-algorithm/zAlgorithm.js
@@ -54,34 +54,22 @@ function zBuilder(zString) {
        */
       zArray[charIndex] = zBoxRightIndex - zBoxLeftIndex;
 
-      // Move right Z box boundary left by one position just because we've used
-      // [zBoxRightIndex - zBoxLeftIndex] index calculation above.
       zBoxRightIndex -= 1;
     } else {
-      // We're INSIDE of Z box.
-
-      // Calculate corresponding Z box shift. Because we want to copy the values
-      // from zArray that have been calculated before.
+      /* We're INSIDE of Z box. */
       zBoxShift = charIndex - zBoxLeftIndex;
 
-      // Check if the value that has been already calculated before
-      // leaves us inside of Z box or it goes beyond the checkbox
-      // right boundary.
       if (zArray[zBoxShift] < (zBoxRightIndex - charIndex) + 1) {
-        // If calculated value don't force us to go outside Z box
-        // then we're safe and we may simply use previously calculated value.
         zArray[charIndex] = zArray[zBoxShift];
       } else {
-        // In case if previously calculated values forces us to go outside of Z box
-        // we can't safely copy previously calculated zArray value. It is because
-        // we are sure that there is no further prefix matches outside of Z box.
-        // Thus such values must be re-calculated and reduced to certain point.
-
-        // To do so we need to shift left boundary of Z box to current position.
+        /**
+         * In case if previously calculated values forces us to go outside of Z box
+         * we can't safely copy previously calculated zArray value. It is because
+         * we are sure that there is no further prefix matches outside of Z box.
+         * Thus such values must be re-calculated and reduced to certain point.
+         */
         zBoxLeftIndex = charIndex;
 
-        // And start comparing characters one by one as we normally do for the case
-        // when we are outside of checkbox.
         while (
           zBoxRightIndex < zString.length
           && zString[zBoxRightIndex - zBoxLeftIndex] === zString[zBoxRightIndex]
@@ -96,29 +84,24 @@ function zBuilder(zString) {
     }
   }
 
-  // Return generated zArray.
   return zArray;
 }
 
 const zAlgorithm = (text, word) => {
-  // The list of word's positions in text. Word may be found in the same text
-  // in several different positions. Thus it is an array.
   const wordPositions = [];
 
-  // Concatenate word and string. Word will be a prefix to a string.
+  /* Concatenate word and string. Word will be a prefix to a string. */
   const zString = `${word}${SEPARATOR}${text}`;
 
-  // Generate Z-array for concatenated string.
+  /* Generate Z-array for concatenated string. */
   const zArray = zBuilder(zString);
 
-  // Based on Z-array properties each cell will tell us the length of the match between
-  // the string prefix and current sub-text. Thus we're may find all positions in zArray
-  // with the number that equals to the length of the word (zString prefix) and based on
-  // that positions we'll be able to calculate word positions in text.
   for (let charIndex = 1; charIndex < zArray.length; charIndex += 1) {
     if (zArray[charIndex] === word.length) {
-      // Since we did concatenation to form zString we need to subtract prefix
-      // and separator lengths.
+      /**
+       * Since we did concatenation to form zString we need to subtract prefix
+       * and separator lengths.
+       */
       const wordPosition = charIndex - word.length - SEPARATOR.length;
 
       wordPositions.push(wordPosition);
@@ -126,3 +109,8 @@ const zAlgorithm = (text, word) => {
   }
   return wordPositions;
 };
+
+console.log(
+  'Positions for "lets" word in "hey ho lets go lets do it" string are',
+  zAlgorithm('hey ho lets go lets do it', 'lets').join(', '),
+);
